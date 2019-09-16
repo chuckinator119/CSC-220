17a891c38efae74278031b13d9aa497277e17e4f
Charlie McBride <38218826+chuckinator119@users.noreply.github.com>
2019-09-16 00:12:00 -0500

    Update Tables.java

--- Assignments/Assignment1/Tables.java
+++ Assignments/Assignment1/Tables.java
@@ -6,8 +6,8 @@ public class Tables{
 
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		File random = new File("C:\\Users\\Charlie\\Documents\\GitHub\\CSC-220\\Assignments\\Assignment1\\random.txt");
-		Scanner scan = new Scanner(random);
+		File rock_jars = new File("C:\\Users\\Charlie\\Documents\\GitHub\\CSC-220\\Assignments\\Assignment1\\rock_jars.txt");
+		Scanner scan = new Scanner(rock_jars);
 
 		System.out.println(scan.nextLine());
 	}
