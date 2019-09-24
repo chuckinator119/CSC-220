e2fd947d955ba3ecd2154d12a00794d7e90248e1
Charlie McBride <38218826+chuckinator119@users.noreply.github.com>
2019-09-24 14:52:35 -0500

    ghjk
    ghkj

--- Assignments/Assignment1/Test.java
+++ Assignments/Assignment1/Test.java
@@ -1,31 +1,6 @@
-import java.util.StringTokenizer;
-import java.io.*;
-
-public class test
-{
-	public static void main(String[] args) throws FileNotFoundException, IOException
-	{
-		FileReader file= new FileReader("rock_jars.txt");
-        BufferedReader bf= new BufferedReader(file);
+public class Test{
+    public static void main(String[] args) {
+        System.out.println("Test");
         
-        String st = bf.readLine();
-        
-        int sum=0;
-        int count=0;
-        while((st=bf.readLine())!=null)
-        {
-            StringTokenizer stn = new StringTokenizer(st);
-            String rn = stn.nextToken();
-            String name = stn.nextToken();
-            int phy = Integer.parseInt(stn.nextToken());
-            int chem = Integer.parseInt(stn.nextToken());
-            int bio = Integer.parseInt(stn.nextToken());
-            int math = Integer.parseInt(stn.nextToken());
-            sum = sum+phy+chem+bio+math;
-            count}++;
-            System.out.println("Roll number="+rn);
-            System.out.println("Name="+name);
-            System.out.printf("Total marks=%d\n", sum);
-            System.out.println("--------------------------------------------------------------------------");
-	   }
-	}
+    }
+}
