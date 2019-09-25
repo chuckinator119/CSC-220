46cbd7f93439643e9cd28c4cc8a51b562d86563d
Charlie McBride <38218826+chuckinator119@users.noreply.github.com>
2019-09-25 17:55:03 -0500

    123

--- Assignments/Assignment1/ArrayMethod.java
+++ Assignments/Assignment1/ArrayMethod.java
@@ -2,15 +2,14 @@ import java.io.File;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class ArrayMethod 
-{
-	public static void main(String[] args)
-	{
+public class ArrayMethod {
+	public static void main(String[] args) {
 		int[] data = readFiles("rock_jars.txt");
 		System.out.println(Arrays.toString(data));
 
 		System.out.println("The length of myArray is" + data.length);
-		System.out.println("--------------------------------\nLeading Digit  Count           %\n--------------------------------");
+		System.out.println(
+				"--------------------------------\nLeading Digit  Count           %\n--------------------------------");
 		System.out.printf("%-10s %-10s %-10s\n", "0", "     t", "       e%");
 		System.out.printf("%-10s %-10s %-10s\n", "1", "     t", "       e%");
 		System.out.printf("%-10s %-10s %-10s\n", "2", "     t", "       e%");
@@ -21,36 +20,31 @@ public class ArrayMethod
 		System.out.printf("%-10s %-10s %-10s\n", "7", "     t", "       e%");
 		System.out.printf("%-10s %-10s %-10s\n", "8", "     t", "       e%");
 		System.out.printf("%-10s %-10s %-10s\n", "9", "     t", "       e%");
-		System.out.println("--------------------------------\nTOTAL          25        100.00%\n--------------------------------\n--------------------------------");		
+		System.out.println(
+				"--------------------------------\nTOTAL          25        100.00%\n--------------------------------\n--------------------------------");
 
-		for (int i = 0; i < data.length; i++)
-		{
-			System.out.println("1")
+		for (int i = 0; i < data.length; i++) {
+			System.out.println("1");
 		}
 
-
 		// for(int count = 0;count < data.length;count++)
-		// 	{
-		// 		first_digit = text;
-		// 	while(first_digit >= 10)
-		// 	{
-		// 		first_digit = first_digit / 10;
+		// {
+		// first_digit = text;
+		// while(first_digit >= 10)
+		// {
+		// first_digit = first_digit / 10;
 
-		// 	}
+		// }
 
 	}
 
+	public static int[] readFiles(String file) {
 
-	public static int[] readFiles(String file)
-	{
-
-		try
-		{
+		try {
 			File f = new File(file);
 			Scanner s = new Scanner(f);
 			int ctr = 0;
-			while(s.hasNextInt())
-			{
+			while (s.hasNextInt()) {
 				ctr++;
 				s.nextInt();
 			}
@@ -59,23 +53,21 @@ public class ArrayMethod
 			for (int i = 0; i < arr.length; i++)
 				arr[i] = s1.nextInt();
 			return arr;
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			return null;
 		}
 
-		// code to calculate the first digit of each newline 
+		// code to calculate the first digit of each newline
 		// int first_digit;
 
 		// for (int i=0; i < arr.length;i++)
 		// {
-		// 	first_digit = text;
-		// 	while(first_digit >= 10)
-		// 	{
-		// 		first_digit = first_digit / 10;
-		// 	}
-		// }	
+		// first_digit = text;
+		// while(first_digit >= 10)
+		// {
+		// first_digit = first_digit / 10;
+		// }
+		// }
 
 	}
 }
