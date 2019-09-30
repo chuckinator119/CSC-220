f658e889801a7b5682eadb67297493e969cae4e2
Charlie McBride <38218826+chuckinator119@users.noreply.github.com>
2019-09-30 09:42:44 -0500

    new coede

--- Notes/9-23-2019/List.java
+++ Notes/9-23-2019/List.java
@@ -27,17 +27,25 @@ class List
 	// clones the list l and sets the last element as the current
 	public List(List l)
 	{
+		list = new char[MAX_SIZE];
+		end = curr = -1;
+	//copy each element
+		for(int i = 0; i < l.GetSize(); i++)
+		InsertAfter(l.list[i]);
+
 	}
 
 	// navigates to the beginning of the list
 	public void First()
 	{
+		curr = 0;
 	}
 
 	// navigates to the end of the list
 	// the end of the list is at the last valid item in the list
 	public void Last()
 	{
+		curr = end;
 	}
 
 	// navigates to the specified element (0-index)
@@ -45,6 +53,8 @@ class List
 	// this should not be possible for invalid positions
 	public void SetPos(int pos)
 	{
+		if(!IsEmpty() && pos <= end &&pos >=0)
+
 	}
 
 	// navigates to the previous element
@@ -52,6 +62,9 @@ class List
 	// there should be no wrap-around
 	public void Prev()
 	{
+		if(!IsEmpty() && curr > 0)
+		curr--;
+
 	}
 
 	// navigates to the next element
@@ -59,27 +72,30 @@ class List
 	// there should be no wrap-around
 	public void Next()
 	{
-		if (!IsEmpty()&& curr != end)
-		curr++; 
+		if(!IsEmpty() && curr < end)
+			curr++;
 	}
 
 	// returns the location of the current element (or -1)
 	public int GetPos()
 	{
-		retrn curr;
+		return curr;
 	}
 
 	// returns the value of the current element (or -1)
 	public char GetValue()
 	{
-		if(isempty)
+		if(IsEmpty())
+			return (char)32;
+		else
+			return list[curr];
 	}
 
 	// returns the size of the list
 	// size does not imply capacity
 	public int GetSize()
 	{
-		return (end + 1)
+		return (end + 1);
 	}
 
 	// inserts an item before the current element
@@ -89,14 +105,17 @@ class List
 	{
 		if(!IsFull())
 		{
+			// if the list is empty
 			if(IsEmpty())
-				curr++
+				curr++;
+			// otherwise shift elements to the right
 			else
 			{
-				//homework	
+				// homework!!!!
 			}
+			// insert the item
 			list[curr] = data;
-			end++
+			end++;			
 		}
 	}
 
@@ -107,12 +126,12 @@ class List
 	{
 		if(!IsFull())
 		{
-			//if we are at the end of the listm just add to the end 
-			if (curr == end)
+			// if we are at the end of the list, just add to the end
+			if(curr == end)
 			{
 				curr++;
 				list[curr] = data;
-				end++
+				end++;
 			}
 			else
 			{
@@ -126,27 +145,57 @@ class List
 	// this should not be possible for an empty list
 	public void Remove()
 	{
+		if(!IsEmpty()){
+		//check if the list is not empty 
+			//if at the end of the list, back everything up one element 
+		if(curr == end){
+			curr--;
+		else{
+			for (int i = curr; i < end; i++)
+				list[i] = list[i+1];
+			
+			}
+		}
 	}
+}
 
 	// replaces the value of the current element with the specified value
 	// this should not be possible for an empty list
 	public void Replace(char data)
 	{
+		if(!IsEmpty())
+			list[curr] = data;
 	}
 
 	// returns if the list is empty
 	public boolean IsEmpty()
 	{
+		
+		return (end==1);
 	}
 
 	// returns if the list is full
 	public boolean IsFull()
 	{
+
+		return (end ==(MAX_SIZE-1));
 	}
 
 	// returns if two lists are equal (by value)
 	public boolean Equals(List l)
 	{
+		//if the lists are not of the same size, they are not equal
+		if(GetSize() != l.GetSize())
+		return false;
+
+		//check element by element of lists of the same size
+		for(int i = 0;i<end;i++)
+		{
+			if(list[i] == l.list[i])
+				return true;
+			else
+				return false;
+		}
 		return true;
 	}
 
@@ -155,25 +204,33 @@ class List
 	// l should be concatenated to the end of *this
 	// the returned list should not exceed MAX_SIZE elements
 	// the last element of the new list is the current
-	public List Add(List 1)
+	public List Add(List l)
 	{
-		return 1;
+		List t = new List(this);
+		if(t.GetSize() + l.GetSize() <= MAX_SIZE)
+		for (int i = 0; i<l.GetSize();i++)
+			t.InsertAfter(l.list[i]);
+
+			return t;
+				
 	}
 
 	// returns a string representation of the entire list (e.g., 1 2 3 4 5)
 	// the string "NULL" should be returned for an empty list
 	public String toString()
 	{
-		//otherwise iterate throughthe list and display each element serperatley 
+		// return "NULL" if the list is empty
 		if(IsEmpty())
 			return "NULL";
+		// otherwise, iterate through the list and display each element separated by a space
 		else
 		{
-			String s;
-			for(int i = 0; i < GetSize();i++)
-				s+= list[i] + " ";
+			String s = " ";
+			
+			for(int i = 0; i < GetSize(); i++)
+				s += list[i] + " ";
+			
 			return s;
 		}
-
 	}
 }
