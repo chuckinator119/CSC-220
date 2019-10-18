2a697aaa4282a3ee36961b6a7e59bf5142957695
Charlie McBride <38218826+chuckinator119@users.noreply.github.com>
2019-10-18 08:28:32 -0500

    123

--- Practice/LLpractice/Node.java
+++ Practice/LLpractice/Node.java
@@ -1,17 +1,56 @@
 class Node {
     public static void main(String[] args) {
-        Node head = new Node();
+        Node head;
         Node tail;
         Node curr;
+
+        head = new Node();
+        tail = head;
+        curr = head;
+        head.setData(0);
+        // 5
+        head.setLink(new Node());
+        tail = head.getLink();
+        tail.setData(5);
+        // 10
+        tail.setLink(new Node());
+        tail = tail.getLink();
+        tail.setData(10);
+        // 15
+        tail.setLink(new Node());
+        tail = tail.getLink();
+        tail.setData(15);
+        // 20
+        tail.setLink(new Node());
+        tail = tail.getLink();
+        tail.setData(20);
+        // removing 15
+
+        // System.out.print(head.getData());
+
+        // curr = head;
+        // curr = curr.getLink().getLink();
+        // curr.setLink(curr.getLink().getLink());
+        // System.out.print(curr.getData());
+        curr = head;
+        Node test = new Node();
+        test.setData(12);
+        curr = curr.getLink().getLink();
+        test.setLink(curr.getLink());
+        curr.setLink(test);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        while (head != null) {
+            System.out.print(head.getData() + " ");
+            head = head.getLink();
+        }
+
     }
 
     private int data;
     private Node link;
-
-    public insert(){
-        Node node = new Node();
-        node.data = data;
-    }
 
     public Node() {
         this.data = 0;
--- Practice/recursion/recur.java
+++ Practice/recursion/recur.java
@@ -3,7 +3,8 @@ public class recur {
 
         // F(20);
         System.out.print(F(20));
-
+        // System.out.print(A(532));
+        // System.out.print(B(900));
     }
 
     static int F(int n) {
@@ -11,4 +12,19 @@ public class recur {
             return 0;
         return 1 + F(n / 2);
     }
+
+    static int A(int n) {
+        if (n <= 0)
+            return 0;
+        return F(n / 10) + n % 10;
+    }
+
+    static int B(int n) {
+        if (n < 0)
+            return F(-n);
+        if (n < 10)
+            return n;
+        return B(n / 10);
+    }
+
 }
