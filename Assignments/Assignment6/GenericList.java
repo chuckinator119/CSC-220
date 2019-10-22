138a395f89c20c042499ee0aa799dee9e9901af6
Charlie McBride <38218826+chuckinator119@users.noreply.github.com>
2019-10-22 00:56:26 -0500

    working on assignment #6

--- Assignments/Assignment6/GenericList.java
+++ Assignments/Assignment6/GenericList.java
@@ -1,50 +1,36 @@
-/* ***************************************************
- * <your name>
- * <the date>
- * <the file name>
- *
- * <a simple, short program/class description>
- *************************************************** */
 
 // the Node class
-class Node<mT>
-{
+class Node<mT> {
 	private mT data;
 	private Node<mT> link;
 
 	// constructor
-	public Node()
-	{
+	public Node() {
 		this.data = null;
 		this.link = null;
 	}
 
 	// accessor and mutator for the data component
-	public mT getData()
-	{
+	public mT getData() {
 		return this.data;
 	}
 
-	public void setData(mT data)
-	{
+	public void setData(mT data) {
 		this.data = data;
 	}
 
 	// accessor and mutator for the link component
-	public Node<mT> getLink()
-	{
+	public Node<mT> getLink() {
 		return this.link;
 	}
 
-	public void setLink(Node<mT> link)
-	{
+	public void setLink(Node<mT> link) {
 		this.link = link;
 	}
 }
 
 // the List class
-public class List<mT>
-{
+public class List<mT> {
 	public static final int MAX_SIZE = 50;
 
 	private Node<mT> head;
@@ -54,52 +40,45 @@ public class List<mT>
 
 	// constructor
 	// remember that an empty list has a "size" of -1 and its "position" is at -1
-	public List()
-	{
+	public List() {
 		this.head = this.tail = this.curr = null;
 		this.num_items = 0;
 	}
 
 	// copy constructor
 	// clones the list l and sets the last element as the current
-	public List(List<mT> l)
-	{
+	public List(List<mT> l) {
 		Node<mT> n = l.head;
 
 		this.head = this.tail = this.curr = null;
 		this.num_items = 0;
 
-		while (n != null)
-		{
+		while (n != null) {
 			this.InsertAfter(n.getData());
 			n = n.getLink();
 		}
 	}
 
 	// navigates to the beginning of the list
-	public void First()
-	{
+	public void First() {
 		this.curr = this.head;
 	}
 
 	// navigates to the end of the list
 	// the end of the list is at the last valid item in the list
-	public void Last()
-	{
+	public void Last() {
 		this.curr = this.tail;
 	}
 
 	// navigates to the specified element (0-index)
 	// this should not be possible for an empty list
 	// this should not be possible for invalid positions
-	public void SetPos(int pos)
-	{
-		if (!this.IsEmpty() && pos >= 0 && pos < this.num_items)
-		{
+	public void SetPos(int pos) {
+		if (!this.IsEmpty() && pos >= 0 && pos < this.num_items) {
 			this.curr = this.head;
 
 			// move curr to the specified position
-			for (int i=0; i<pos; i++)
+			for (int i = 0; i < pos; i++)
 				this.curr = this.curr.getLink();
 		}
 	}
@@ -107,10 +86,8 @@ public class List<mT>
 	// navigates to the previous element
 	// this should not be possible for an empty list
 	// there should be no wrap-around
-	public void Prev()
-	{
-		if (!this.IsEmpty() && this.curr != this.head)
-		{
+	public void Prev() {
+		if (!this.IsEmpty() && this.curr != this.head) {
 			Node<mT> n = this.head;
 
 			// move n to the previous element
@@ -124,15 +101,13 @@ public class List<mT>
 	// navigates to the next element
 	// this should not be possible for an empty list
 	// there should be no wrap-around
-	public void Next()
-	{
+	public void Next() {
 		if (!this.IsEmpty() && this.curr != this.tail)
 			this.curr = this.curr.getLink();
 	}
 
 	// returns the location of the current element (or -1)
-	public int GetPos()
-	{
+	public int GetPos() {
 		if (this.IsEmpty())
 			return -1;
 
@@ -140,8 +115,7 @@ public class List<mT>
 		int i = 0;
 
 		// traverse the list to get the current position
-		while (n != this.curr)
-		{
+		while (n != this.curr) {
 			n = n.getLink();
 			i++;
 		}
@@ -150,8 +124,7 @@ public class List<mT>
 	}
 
 	// returns the value of the current element (or -1)
-	public mT GetValue()
-	{
+	public mT GetValue() {
 		if (this.IsEmpty())
 			return null;
 		else
@@ -160,26 +133,22 @@ public class List<mT>
 
 	// returns the size of the list
 	// size does not imply capacity
-	public int GetSize()
-	{
+	public int GetSize() {
 		return this.num_items;
 	}
 
 	// inserts an item before the current element
 	// the new element becomes the current
 	// this should not be possible for a full list
-	public void InsertBefore(mT data)
-	{
-		if (!this.IsFull())
-		{
+	public void InsertBefore(mT data) {
+		if (!this.IsFull()) {
 			// if the list is empty, just insert after
 			if (this.IsEmpty())
 				this.InsertAfter(data);
-			else
-			{
-				// if we're at the beginning, just create a new head that points to the current one
-				if (this.curr == this.head)
-				{
+			else {
+				// if we're at the beginning, just create a new head that points to the current
+				// one
+				if (this.curr == this.head) {
 					this.head = new Node<mT>();
 					this.head.setData(data);
 					this.head.setLink(curr);
@@ -187,8 +156,7 @@ public class List<mT>
 					this.num_items++;
 				}
 				// otherwise, navigate to the previous node and insert after
-				else
-				{
+				else {
 					this.Prev();
 					this.InsertAfter(data);
 				}
@@ -199,10 +167,8 @@ public class List<mT>
 	// inserts an item after the current element
 	// the new element becomes the current
 	// this should not be possible for a full list
-	public void InsertAfter(mT data)
-	{
-		if (!this.IsFull())
-		{
+	public void InsertAfter(mT data) {
+		if (!this.IsFull()) {
 			Node<mT> n = new Node<mT>();
 
 			n.setData(data);
@@ -210,17 +176,14 @@ public class List<mT>
 			// if the list is empty, everything points to the single node
 			if (this.IsEmpty())
 				this.head = this.tail = this.curr = n;
-			else
-			{
+			else {
 				// if we're at the end, just tack the new node on
-				if (this.curr == this.tail)
-				{
+				if (this.curr == this.tail) {
 					this.curr.setLink(n);
 					this.curr = this.tail = n;
 				}
 				// otherwise, change the links to insert the node
-				else
-				{
+				else {
 					n.setLink(this.curr.getLink());
 					this.curr.setLink(n);
 					this.curr = n;
@@ -233,13 +196,10 @@ public class List<mT>
 
 	// removes the current element (collapsing the list)
 	// this should not be possible for an empty list
-	public void Remove()
-	{
-		if (!this.IsEmpty())
-		{
+	public void Remove() {
+		if (!this.IsEmpty()) {
 			// if we're at the beginning, reset the head
-			if (this.curr == this.head)
-			{
+			if (this.curr == this.head) {
 				this.head = this.curr = this.curr.getLink();
 
 				// if we just removed the last node in the list
@@ -247,8 +207,7 @@ public class List<mT>
 					this.tail = null;
 			}
 			// otherwise, go back a node and reroute around the node to be removed
-			else
-			{
+			else {
 				this.Prev();
 				// and rearrange the pointer to remove this node
 				this.curr.setLink(this.curr.getLink().getLink());
@@ -263,27 +222,23 @@ public class List<mT>
 
 	// replaces the value of the current element with the specified value
 	// this should not be possible for an empty list
-	public void Replace(mT data)
-	{
+	public void Replace(mT data) {
 		if (!this.IsEmpty())
 			this.curr.setData(data);
 	}
 
 	// returns if the list is empty
-	public boolean IsEmpty()
-	{
+	public boolean IsEmpty() {
 		return (this.head == null);
 	}
 
 	// returns if the list is full
-	public boolean IsFull()
-	{
+	public boolean IsFull() {
 		return (this.num_items == MAX_SIZE);
 	}
 
 	// returns if two lists are equal (by value)
-	public boolean Equals(List<mT> l)
-	{
+	public boolean Equals(List<mT> l) {
 		// the lists are not equal if they're of different sizes
 		if (this.num_items != l.num_items)
 			return false;
@@ -292,8 +247,7 @@ public class List<mT>
 		Node<mT> q = l.head;
 
 		// iterate through each list
-		while (p != null)
-		{
+		while (p != null) {
 			// if any pair of elements differ, the lists are not equal
 			if (p.getData() != q.getData())
 				return false;
@@ -309,15 +263,13 @@ public class List<mT>
 	// l should be concatenated to the end of *this
 	// the returned list should not exceed MAX_SIZE elements
 	// the last element of the new list is the current
-	public List<mT> Add(List<mT> l)
-	{
+	public List<mT> Add(List<mT> l) {
 		// copy the first list
 		List<mT> t = new List<mT>(this);
 		Node<mT> n = l.head;
 
 		// iterate through the second list and copy each element to the new list
-		while (n != null && !t.IsFull())
-		{
+		while (n != null && !t.IsFull()) {
 			t.InsertAfter(n.getData());
 			n = n.getLink();
 		}
@@ -327,19 +279,17 @@ public class List<mT>
 
 	// returns a string representation of the entire list (e.g., 1 2 3 4 5)
 	// the string "NULL" should be returned for an empty list
-	public String toString()
-	{
+	public String toString() {
 		// "NULL" if the list is empty
 		if (this.head == null)
 			return "NULL";
-		else
-		{
+		else {
 			Node<mT> n = this.head;
 			String s = "";
 
-			// otherwise iterate through the list and display each element separated by a space
-			while (n != null)
-			{
+			// otherwise iterate through the list and display each element separated by a
+			// space
+			while (n != null) {
 				s += n.getData() + " ";
 				n = n.getLink();
 			}
--- Assignments/Assignment6/StackTest.java
+++ Assignments/Assignment6/StackTest.java
@@ -2,10 +2,8 @@
  * YOU MAY NOT CHANGE ANYTHING BELOW THIS!
  **************************************** */
 
-public class StackTest
-{
-	public static void main(String[] args)
-	{
+public class StackTest {
+	public static void main(String[] args) {
 		Stack<Integer> s1 = new Stack<Integer>();
 
 		System.out.println("*declare stack s1\ns1=" + s1); // stack initially set to 0
@@ -15,8 +13,8 @@ public class StackTest
 		System.out.println("s1.Peek()=" + s1.Peek());
 		System.out.println();
 
-		for (int i=0; i<1000; i++) // push 1000 times with i+100
-			s1.Push(i+100);
+		for (int i = 0; i < 1000; i++) // push 1000 times with i+100
+			s1.Push(i + 100);
 		System.out.println("*Push(i+100) 1000 times\ns1=" + s1);
 		System.out.println("s1.Size()=" + s1.Size());
 		System.out.println("s1.IsEmpty()=" + ((s1.IsEmpty()) ? "T" : "F"));
@@ -24,7 +22,7 @@ public class StackTest
 		System.out.println("s1.Peek()=" + s1.Peek());
 		System.out.println();
 
-		for (int i=0; i<10; i++) // pop 10 times
+		for (int i = 0; i < 10; i++) // pop 10 times
 			s1.Pop();
 		System.out.println("*pop 10 times\ns1=" + s1);
 		System.out.println("s1.Size()=" + s1.Size());
@@ -33,7 +31,7 @@ public class StackTest
 		System.out.println("s1.Peek()=" + s1.Peek());
 		System.out.println();
 
-		for (int i=0; i<1000; i++) // pop 1000 times
+		for (int i = 0; i < 1000; i++) // pop 1000 times
 			s1.Pop();
 		System.out.println("*pop 1000 times\ns1=" + s1);
 		System.out.println("s1.Size()=" + s1.Size());
@@ -42,8 +40,8 @@ public class StackTest
 		System.out.println("s1.Peek()=" + s1.Peek());
 		System.out.println();
 
-		for (int i=0; i<10; i++) // push 10 times with i*i
-			s1.Push(i*i);
+		for (int i = 0; i < 10; i++) // push 10 times with i*i
+			s1.Push(i * i);
 		System.out.println("*push 10 times with i*i\ns1=" + s1);
 		System.out.println("s1.Size()=" + s1.Size());
 		System.out.println("s1.IsEmpty()=" + ((s1.IsEmpty()) ? "T" : "F"));
@@ -90,7 +88,7 @@ public class StackTest
 		System.out.println("s1.Peek()=" + s1.Peek());
 		System.out.println();
 
-		for (int i=0; i<5; i++)
+		for (int i = 0; i < 5; i++)
 			s1.Pop();
 		System.out.println("*pop s1 5 times");
 		System.out.println("s1=" + s1 + "\ns2=" + s2 + "\ns3=" + s3);
@@ -119,7 +117,7 @@ public class StackTest
 		System.out.println("s1=" + s1 + "\ns2=" + s2 + "\ns3=" + s3);
 		System.out.println("s3.Peek()=" + s3.Peek());
 		System.out.println();
-		for (int i=8; i>0; i--)
+		for (int i = 8; i > 0; i--)
 			s3.Push(i);
 		System.out.println("*push s3 8 times with i (backwards)\ns3=" + s3);
 		System.out.println("s3.Peek()=" + s3.Peek());
@@ -133,7 +131,7 @@ public class StackTest
 		System.out.println();
 
 		Stack<Character> s4 = new Stack<Character>();
-		for (char c='a'; c<='z'; c++)
+		for (char c = 'a'; c <= 'z'; c++)
 			s4.Push(c);
 		System.out.println("s4=" + s4);
 		System.out.println("s4.Size()=" + s4.Size());
